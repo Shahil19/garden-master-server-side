9e772cd97a0f114efd35685fa49ccea2f4cd804b
shahil19 <basharshahil@gmail.com>
2022-05-28 11:27:33 +0600

    order api removed for heroku error

--- index.js
+++ index.js
@@ -67,17 +67,7 @@ async function run() {
             res.send(result)
         })
 
-        // orders
-        app.put("/order/:id", async (req, res) => {
-            const id = req.params.id
-            const filter = { _id: ObjectId(id) }
-            const order = req.body
-            const updateDoc = {
-                $set: { order }
-            };
-            const result = await orderCollection.insertOne(filter, updateDoc)
-            res.send(result)
-        })
+
 
         console.log("connected");
     } finally {
